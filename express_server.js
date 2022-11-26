e5a9d2c4288d85dfe1fa840fe8bbcf748857422f
ophir <ophiryoram@gmail.com>
2022-11-26 17:58:18 +0200

    created People, Groups,Doors and Permissions pages, also made a new Home page. Also added footer, header and navbar ejs partials

--- express_server.js
+++ express_server.js
@@ -2,36 +2,25 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("views", path.join(__dirname, "/views"));
+
+// Get logic
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
 });
-
-app.get("/search", (req, res) => {
-  const { q } = req.query;
-  if (!q) {
-    res.send(`Nothing searched`);
-  } else {
-    res.send(`this is your query: ${q}!!!`);
-  }
+app.get("/people", (req, res) => {
+  res.render("people");
 });
-
-app.get("/r/:sub", (req, res) => {
-  const { sub } = req.params;
-  res.send(`this is sub: ${sub}!!!`);
+app.get("/groups", (req, res) => {
+  res.render("groups");
 });
-app.get("/r/:sub/:next", (req, res) => {
-  const { sub } = req.params;
-  res.send(`this is sub: ${sub}!!!`);
+app.get("/doors", (req, res) => {
+  res.render("doors");
 });
-
-app.get("/cats", (req, res) => {
-  res.send("meow");
-});
-app.get("/dogs", (req, res) => {
-  res.send("woof");
+app.get("/permissions", (req, res) => {
+  res.render("permissions");
 });
 
 // reply to any other path
@@ -39,13 +28,7 @@ app.get("*", (req, res) => {
   res.send("not a valid path");
 });
 
-app.post("/cats", (req, res) => {
-  res.send("we got post!");
-});
-// app.use((req, res) => {
-//   console.log("got a request!");
-// });
-
+// listen logic
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
---
+++ public/js/formTableLogic.js
@@ -0,0 +1,75 @@
+function swapHoveredClass() {
+  this.classList.replace(`${this.dataset.origClass}`, "hoveredOver");
+}
+
+function returnOrigClass() {
+  this.classList.replace("hoveredOver", `${this.dataset.origClass}`);
+}
+
+function tableRowCreate(pokeid, is_even) {
+  if (isNaN(pokeid) || pokeid > 905 || pokeid < 1) {
+    alert("must enter a number between 1-905");
+    return true;
+  }
+
+  // image link
+  pkmnLink =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+  // picking the container to be used
+  const container = document.querySelector("#container");
+  // create row element
+  const Person = document.createElement("tr");
+  const ID = document.createElement("td");
+  const newimg = document.createElement("td");
+  const Type = document.createElement("td");
+  // give row element an id, 2 event listeners
+  Person.id = pokeid;
+  Person.addEventListener("mouseenter", swapHoveredClass);
+  Person.addEventListener("mouseleave", returnOrigClass);
+
+  // give the tr a class depending on its evenness and define original class
+  if (!is_even) {
+    Person.classList.add("odd");
+    Person.dataset.origClass = "odd";
+  } else {
+    Person.classList.add("even");
+    Person.dataset.origClass = "even";
+  }
+  Type.innerText = "type";
+  // add lable
+  const pokeNum = document.createElement("span");
+  pokeNum.innerText = "#" + pokeid;
+  ID.appendChild(pokeNum);
+  //add the source image
+  const newImg = document.createElement("img");
+  // newImg.src = ".png";
+  newImg.src = pkmnLink + `${pokeid}` + ".png";
+  newimg.appendChild(newImg);
+
+  // append all we created.
+  Person.appendChild(ID);
+  Person.appendChild(newimg);
+  Person.appendChild(Type);
+  container.appendChild(Person);
+}
+
+/*#################################### FORM START ##########################################*/
+const form = document.querySelector("#addPokeForm");
+const input = document.querySelector("#pokeInputText");
+let is_even = true;
+window.onload = () => {
+  document.getElementById("pokeInputText").focus();
+};
+console.log(tableRowCreate);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const pokeNum = input.value;
+  if (tableRowCreate(pokeNum, is_even)) {
+    console.log("no pokemon belongs to that ID!!!, try again!!");
+  } else {
+    is_even = !is_even;
+  }
+  input.value = "";
+  document.getElementById("pokeInputText").focus();
+});
+/*#################################### FORM END ##########################################*/
