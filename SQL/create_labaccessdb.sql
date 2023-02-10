fcd895ceb460dcd367b6f8dd8f6795ce03e386dd
ophir054 <ophiryoram@campus.technion.ac.il>
2023-02-10 11:57:08 +0200

    Added MYSQL to the git

---
+++ SQL/create_labaccessdb.sql
@@ -0,0 +1,194 @@
+
+-- People table #########################################################################
+CREATE TABLE `People`(
+    `person_id` INT NOT NULL,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    `admin_system_access` TINYINT(1) NOT NULL,
+    `admin_password` VARCHAR(255) NULL COMMENT 'Only active if person is an admin'
+  );
+ALTER TABLE
+  `People`
+ADD
+  PRIMARY KEY (`person_id`);
+
+
+-- People_Groups table #########################################################################
+CREATE TABLE `People_Groups`(
+    `person_group_id` INT NOT NULL,
+    `person_id` INT NOT NULL,
+		FOREIGN KEY (`person_id`)
+		REFERENCES `People` (`person_id`)
+		ON DELETE CASCADE,
+    `person_group_name` VARCHAR(255) NOT NULL
+  );
+ALTER TABLE
+  `People_Groups`
+ADD
+  PRIMARY KEY (`person_group_id`, `person_id`);
+
+
+-- Doors table #########################################################################
+CREATE TABLE `Doors`(
+    `door_id` INT NOT NULL,
+    `door_name` VARCHAR(255) NOT NULL,
+    `building_name` VARCHAR(255) NOT NULL,
+    `floor_number` INT NOT NULL
+  );
+ALTER TABLE
+  `Doors`
+ADD
+  PRIMARY KEY (`door_id`);
+
+
+-- Door_Groups table #########################################################################
+CREATE TABLE `Door_Groups`(
+    `door_group_id` INT NOT NULL,
+    `door_id` INT NOT NULL,
+	FOREIGN KEY (`door_id`)
+		REFERENCES `Doors` (`door_id`)
+		ON DELETE CASCADE,
+    `door_group_name` VARCHAR(255) NOT NULL
+  );
+ALTER TABLE
+  `Door_Groups`
+ADD
+  PRIMARY KEY (`door_group_id`, `door_id`);
+  
+  
+-- Permissions_table table #########################################################################
+CREATE TABLE `Permissions_table`(
+    `permission_id` INT NOT NULL,
+    `permission_type` VARCHAR(255) NOT NULL DEFAULT 'access' COMMENT 'permission type:
+
+\"Door Access\" - grants access to a door.
+
+\"Admin Privileges\" - grants admin privilages to a door.
+
+\"Admin Add People\" - permitts adding people to the system.
+
+\"Admin Add Doors\" - permitts adding doors to the system.
+
+\"Admin Add Permissions_table\" - permitts adding permissions to people for specific doors.
+
+\"Admin Add Admin\" - grants permit to add another admin to a door.',
+    `initial_date` DATE NOT NULL COMMENT 'When does the permit start to take effect',
+    `expiry_date` DATE NOT NULL COMMENT 'When does the permit expire',
+    `start_time` TIME NOT NULL COMMENT 'When does the permit start to take effect every day.',
+    `end_time` TIME NOT NULL COMMENT 'When does the permit end every day.'
+  );
+ALTER TABLE
+  `Permissions_table`
+ADD
+  PRIMARY KEY (`permission_id`);
+
+
+-- Permission_Groups table #########################################################################
+CREATE TABLE `Permission_Groups`(
+    `permission_group_id` INT NOT NULL,
+    `permission_id` INT NOT NULL,
+	FOREIGN KEY (`permission_id`)
+		REFERENCES `Permissions_table` (`permission_id`)
+		ON DELETE CASCADE,
+    `permission_group_name` VARCHAR(255) NOT NULL
+  );
+ALTER TABLE
+  `Permission_Groups`
+ADD
+  PRIMARY KEY (`permission_group_id`, `permission_id`);
+
+
+-- Peoples_Permissions_Doors table #########################################################################
+CREATE TABLE `Peoples_Permissions_Doors`(
+    `person_group_id` INT NOT NULL,
+    FOREIGN KEY (person_group_id)
+	REFERENCES `People_Groups` (`person_group_id`)
+	ON DELETE CASCADE,
+	
+    `door_group_id` INT NOT NULL,
+    FOREIGN KEY (`door_group_id`)
+	    REFERENCES `Door_Groups` (`door_group_id`)
+	    ON DELETE CASCADE,
+    `permission_group_id` INT NOT NULL,
+    FOREIGN KEY (`permission_group_id`)
+	REFERENCES `Permission_Groups` (`permission_group_id`)
+	ON DELETE CASCADE,
+
+    `permission_group_name` VARCHAR(255) NOT NULL,
+    `person_group_name` VARCHAR(255) NOT NULL,
+    `door_group_name` VARCHAR(255) NOT NULL
+  );
+ALTER TABLE
+  `Peoples_Permissions_Doors`
+ADD
+  PRIMARY KEY (
+    `person_group_id`,
+    `permission_group_id`,
+    `door_group_id`
+  );
+
+  
+
+
+
+
+
+
+
+
+CREATE TABLE `Door_Permission_list`(
+  `id` INT NOT NULL,
+  `person_id` INT NOT NULL,
+  `door_id` INT NOT NULL,
+  `permission_id` INT NOT NULL,
+  `permission_type` VARCHAR(255) NOT NULL,
+  `start_date` DATE NOT NULL,
+  `expiry_date` DATE NOT NULL,
+  `initial_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  `entrence_password` VARCHAR(255) NULL DEFAULT '000000' COMMENT 'Enter a 6 digit pin-code which includes digits 0-9, \"A\",\"B\",\"C\",\"D\",\"*\",\"#\".'
+);
+ALTER TABLE
+  `Door_Permission_list`
+ADD
+  PRIMARY KEY (`id`);
+  
+CREATE TABLE `Creation_Archive`(
+    `id` INT NOT NULL,
+    `admin_id` INT NOT NULL,
+    `entity_type` VARCHAR(255) NOT NULL,
+    `entity_id` INT NOT NULL,
+    `time` TIME NOT NULL,
+    `date` TIMESTAMP NOT NULL
+  );
+ALTER TABLE
+  `Creation_Archive`
+ADD
+  PRIMARY KEY (`id`);
+  
+CREATE TABLE `Permission_Archive`(
+    `id` INT NOT NULL,
+    `person_id` INT NOT NULL,
+    `admin_id` INT NOT NULL,
+    `door_id` INT NOT NULL,
+    `permission_id` INT NOT NULL,
+    `date` TIMESTAMP NOT NULL,
+    `time` TIME NOT NULL
+  );
+ALTER TABLE
+  `Permission_Archive`
+ADD
+  PRIMARY KEY `permission_archive_id_primary`(`id`);
+  
+  
+CREATE TABLE `Access_History`(
+    `id` INT NOT NULL,
+    `door_id` INT NOT NULL,
+    `person_id` INT NOT NULL,
+    `date` TIMESTAMP NOT NULL,
+    `entrence_method` VARCHAR(255) NOT NULL
+  );
+ALTER TABLE
+  `Access_History`
+ADD
+  PRIMARY KEY `access_history_id_primary`(`id`);
---
+++ SQL/database script.sql
@@ -0,0 +1,188 @@
+-- People scripts:#############################################################################
+-- create person
+
+-- INSERT INTO people()
+--     VALUES(
+--         123456789, -- door_groups
+--         'Ophir', -- first_name
+--         'Yoram', -- last_name
+--         1, -- admin_system_access
+--         '159951' -- admin_password / not neccecery if admin_system_access=0 
+--     );
+INSERT INTO
+  people()
+VALUES(
+    123456789,
+    'Ophir',
+    'Yoram',
+    1,
+    '159951'
+  ),
+  (
+    321456987,
+    'Ben',
+    'Rote',
+    1,
+    '159951'
+  ),
+  (
+    987456321,		
+    'Noam',
+    'Barir',
+    0,
+	''
+  );
+-- update person
+
+
+-- delete person
+DELETE FROM people where person_id = ...
+  
+  
+  
+  -- people_groups scripts:#############################################################################
+  -- create people_groups
+  -- INSERT INTO people_groups()
+  --     VALUES(
+  --         321, -- person_group_id
+  --         123456789, -- person_id
+  --         'lab1a people group', -- person_group_name
+  --     );
+INSERT INTO
+  people_groups()
+VALUES(
+    321,
+    123456789,
+    'lab1a people group'
+  );
+-- update people_groups
+  -- delete people_groups
+DELETE FROM people_groups where people_group_id = ...  
+  
+  
+
+  -- doors scripts:#############################################################################
+  -- create doors
+  -- INSERT INTO doors()
+  --     VALUES(
+  --         12, -- door_id
+  --         'lab1a door', -- door_name
+  --         'Ulman', -- building_name
+  --         1 -- floor_number
+  --     );
+INSERT INTO
+  doors()
+VALUES
+(
+12,
+'lab1a door',
+'Ulman',
+1
+),
+(11,
+'lab1b door',
+'Ulman',
+1
+);
+-- update doors
+-- delete doors
+DELETE FROM doors where door_id = ...  
+  -- door_groups scripts:#############################################################################
+  -- create door_groups
+  -- INSERT INTO door_groups()
+  --     VALUES(
+  --         112, -- door_group_id
+  --         12, -- door_id
+  --         'lab1a door group'-- door_group_name
+  --     );
+INSERT INTO
+  door_groups()
+VALUES(112, 12, 'lab1a door group');
+-- update door_groups
+-- delete door_groups
+DELETE FROM door_groups where door_group_id = ...  
+
+
+
+
+  -- permissions scripts:#############################################################################
+  -- create permissions
+  -- INSERT INTO permissions()
+  --     VALUES(
+  --         21, -- permissions_id
+  --         'Door Access', -- permission_type
+  -- --         \'Door Access\' - grants access to a door.
+  -- --          \'Admin Privileges\' - grants admin privilages to a door.
+  -- --          \'Admin Add People\' - permitts adding people to the system.
+  -- --          \'Admin Add Doors\' - permitts adding doors to the system.
+  -- --          \'Admin Add Permissions\' - permitts adding permissions to people for specific doors.
+  -- --          \'Admin Add Admin\' - grants permit to add another admin to a door.',
+  --         '2021-07-26', -- initial_date
+  --         '2021-07-29', -- expiry_date
+  --         '05:05:05' -- start_time
+  --         '15:05:05' -- end_time
+  --     );
+INSERT INTO
+  permissions_table()
+VALUES(
+    21,
+    'Door Access',
+    '2021-07-26',
+    '2021-07-29',
+    '05:05:05',
+    '15:05:05'
+  );
+-- update permissions
+
+-- delete permissions
+DELETE FROM permissions_table where permissions_id = ...  
+  
+  
+  
+  -- permission_groups scripts:#############################################################################
+  -- create permission_groups
+  -- INSERT INTO permission_groups()
+  --     VALUES(
+  --         221, -- door_group_id
+  --         21, -- door_id
+  --         'lab1a permissions group' -- door_group_name
+  --     );
+INSERT INTO
+  permission_groups()
+VALUES(
+    221,
+    21,
+    'lab1a permissions group'
+  );
+-- update permission_groups
+
+-- delete permission_groups
+DELETE FROM permission_groups where permission_group_id = ...
+  
+  
+  
+  
+  -- peoples_permissions_doors scripts:#############################################################################
+  -- create peoples_permissions_doors
+  -- INSERT INTO peoples_permissions_doors()
+  --     VALUES(
+  --             321--  person_group_id
+  --           112 --  door_group_id
+  --           221 --  permission_group_id
+  --           'lab1a permissions group' --  permission_group_name
+  --             'lab1a people group'--  person_group_name
+  --             'lab1a door group'--  door_group_name
+  --     );
+INSERT INTO
+  peoples_permissions_doors()
+VALUES(
+    321,
+	112,
+    221,
+    'lab1a permissions group',
+    'lab1a people group',
+    'lab1a door group'
+  );
+-- update peoples_permissions_doors
+
+-- delete peoples_permissions_doors
